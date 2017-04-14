838771e293b80c6410ae8d59541d84067bf20eaa
Dan Moutal <dan@moutal.ca>
2017-04-13 23:18:50 -0700

    update ZFS snapshot cleaner to work on specific datasets

--- scripts/Dangerous_Scripts/zfs_snapshot_cleaner.sh
+++ scripts/Dangerous_Scripts/zfs_snapshot_cleaner.sh
@@ -3,16 +3,16 @@
 # Deletes ALL zfs snapshots. Use with care!
 
 echo "Are You Sure?"
-read -r -p "Selecting yes will delete ALL snapshots in the zfs pool [YES/NO] " input
+read -r -p "Selecting yes will delete ALL snapshots [YES/NO] " input
 
 case $input in
-	YES)
-		echo "WARNING: Deleting all pool snapshots in 10 seconds. Hit ctrl+c to cancel"
-		sleep 10
-		zfs list -H -o name -t snapshot -r | xargs -n1 -t zfs destroy 
-		;;
-	*)
-	echo "Cowardly refusing to proceed any further!"
-	exit 1
-	;;
+    YES)
+        echo "WARNING: Deleting all snapshots in 10 seconds. Hit ctrl+c to cancel"
+        sleep 10
+        zfs list -H -o name -t snapshot -r $1 | xargs -n1 -t zfs destroy 
+        ;;
+    *)
+    echo "Cowardly refusing to proceed any further!"
+    exit 1
+    ;;
 esac
