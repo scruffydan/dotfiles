6e49906ffd9088bd22aa439ce83e040900129864
Dan Moutal <dan@moutal.ca>
2018-05-18 23:20:13 -0700

    update nvim install script

--- nvim-install.sh
+++ nvim-install.sh
@@ -3,3 +3,7 @@
 # Setup neovim config file 
 mkdir -p ~/.config/nvim/ && touch ~/.config/nvim/init.vim;
 printf "source ~/dotfiles/nvimrc" >> ~/.config/nvim/init.vim;
+
+# Setup vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
