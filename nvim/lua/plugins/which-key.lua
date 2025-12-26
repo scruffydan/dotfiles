91823df215f2514a7e0d568ca90a765309326749
Dan Moutal <dan@moutal.ca>
2025-12-25 21:32:29 -0800

    which-key: add helix preset, group specs, and buffer-local keymap viewer

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,6 +2,25 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   config = function()
-    require("which-key").setup({})
+    require("which-key").setup({
+      preset = "helix",
+      spec = {
+        { "<leader>f", group = "file/find" },
+        { "<leader>g", group = "git" },
+        { "<leader>l", group = "lsp" },
+        { "<leader>a", group = "ai/sidekick" },
+        { "<leader>t", group = "toggle" },
+        { "<leader>d", group = "diff" },
+      },
+    })
   end,
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer local keymaps",
+    },
+  },
 }
