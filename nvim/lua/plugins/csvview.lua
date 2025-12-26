7cc8c589901696134192f48a3d8b4b07a5ae539c
Dan Moutal <dan@moutal.ca>
2025-12-25 20:59:58 -0800

    csvview: add comment parser for # and // prefixed lines

--- nvim/lua/plugins/csvview.lua
+++ nvim/lua/plugins/csvview.lua
@@ -2,6 +2,9 @@ return {
   "hat0uma/csvview.nvim",
   ft = { "csv", "tsv" },
   opts = {
+    parser = {
+      comments = { "#", "//" },
+    },
     view = {
       display_mode = "border",
     },
