784ed8fc7ff74c147ad1290e8f12cf39f4908d79
Dan Moutal <dan@moutal.ca>
2025-12-27 14:15:28 -0800

    nvim: fix treesitter-textobjects to use main branch API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,57 +7,90 @@ return {
     "mason-org/mason.nvim", -- Mason provides tree-sitter-cli
     {
       "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
       config = function()
         require("nvim-treesitter-textobjects").setup({
           select = {
-            -- Enable text object selection
             lookahead = true, -- Jump forward to matching text object
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["al"] = "@loop.outer",
-              ["il"] = "@loop.inner",
-              ["ai"] = "@conditional.outer",
-              ["ii"] = "@conditional.inner",
-              ["a/"] = "@comment.outer",
-              ["i/"] = "@comment.inner",
-            },
           },
           move = {
-            -- Enable movement between text objects
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-              ["]a"] = "@parameter.inner",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-              ["[a"] = "@parameter.inner",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-            },
-          },
-          swap = {
-            -- Enable swapping of text objects
-            swap_next = {
-              ["<leader>sn"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>sp"] = "@parameter.inner",
-            },
+            set_jumps = true, -- Set jumps in the jumplist
           },
         })
+
+        local select = require("nvim-treesitter-textobjects.select")
+        local move = require("nvim-treesitter-textobjects.move")
+        local swap = require("nvim-treesitter-textobjects.swap")
+
+        -- Text object selection keymaps
+        local select_keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["ai"] = "@conditional.outer",
+          ["ii"] = "@conditional.inner",
+          ["a/"] = "@comment.outer",
+          ["i/"] = "@comment.inner",
+        }
+        for key, query in pairs(select_keymaps) do
+          vim.keymap.set({ "x", "o" }, key, function()
+            select.select_textobject(query, "textobjects")
+          end, { desc = "Select " .. query })
+        end
+
+        -- Movement keymaps
+        local goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+          ["]a"] = "@parameter.inner",
+        }
+        local goto_next_end = {
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
+        }
+        local goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+          ["[a"] = "@parameter.inner",
+        }
+        local goto_previous_end = {
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
+        }
+
+        for key, query in pairs(goto_next_start) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            move.goto_next_start(query, "textobjects")
+          end, { desc = "Next " .. query .. " start" })
+        end
+        for key, query in pairs(goto_next_end) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            move.goto_next_end(query, "textobjects")
+          end, { desc = "Next " .. query .. " end" })
+        end
+        for key, query in pairs(goto_previous_start) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            move.goto_previous_start(query, "textobjects")
+          end, { desc = "Previous " .. query .. " start" })
+        end
+        for key, query in pairs(goto_previous_end) do
+          vim.keymap.set({ "n", "x", "o" }, key, function()
+            move.goto_previous_end(query, "textobjects")
+          end, { desc = "Previous " .. query .. " end" })
+        end
+
+        -- Swap keymaps
+        vim.keymap.set("n", "<leader>sn", function()
+          swap.swap_next("@parameter.inner")
+        end, { desc = "Swap parameter with next" })
+        vim.keymap.set("n", "<leader>sp", function()
+          swap.swap_previous("@parameter.inner")
+        end, { desc = "Swap parameter with previous" })
       end,
     },
   },
