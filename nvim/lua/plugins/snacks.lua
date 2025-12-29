6a992f3a9ad1a5188d65188fa63812bceabba570
Dan Moutal <dan@moutal.ca>
2025-12-29 10:18:36 -0800

    nvim: reorganize snacks keymaps into logical groups
    Group keymaps by prefix with section comments:
    - Top-level (<leader><space>, <leader>e)
    - Find/workspace (<leader>f*)
    - Search/buffer (<leader>s*)
    - Git (<leader>g*)
    - GitHub (<leader>gh*)
    - LSP (<leader>l*)
    
    Each section sorted alphabetically by key.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -87,48 +87,57 @@ return {
     })
   end,
   keys = {
-    { "<leader>sn", function() Snacks.notifier.show_history() end, desc = "Notification history" },
-    { "<leader>go", function() Snacks.gitbrowse() end, desc = "Open in browser (GitHub)" },
+    -- Top-level
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart find files" },
+    { "<leader>e", function() Snacks.picker.explorer() end, desc = "File explorer" },
+
+    -- Find (workspace)
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics (workspace)" },
     { "<leader>ff", function() Snacks.picker.files() end, desc = "Find files" },
     { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live grep" },
-    { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep open buffers" },
-    { "<leader>fw", function() Snacks.picker.grep_word() end, desc = "Grep word", mode = { "n", "x" } },
-    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fh", function() Snacks.picker.recent() end, desc = "Recent files" },
-    { "<leader>sr", function() Snacks.picker.registers() end, desc = "Registers" },
-    { "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Diagnostics (buffer)" },
-    { "<leader>fd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics (workspace)" },
-    { "<leader>li", function() Snacks.picker.lsp_incoming_calls() end, desc = "Incoming calls" },
-    { "<leader>lo", function() Snacks.picker.lsp_outgoing_calls() end, desc = "Outgoing calls" },
-    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
-    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man pages" },
-    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help tags" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     { "<leader>fq", function() Snacks.picker.qflist() end, desc = "Quickfix list" },
-    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jump list" },
-    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
-    { "<leader>sp", function() Snacks.picker.pickers() end, desc = "Pickers" },
+    { "<leader>fw", function() Snacks.picker.grep_word() end, desc = "Grep word", mode = { "n", "x" } },
+
+    -- Search (buffer/local)
+    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Search buffer" },
+    { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep open buffers" },
     { "<leader>sc", function() Snacks.picker.commands() end, desc = "Commands" },
     { "<leader>sC", function() Snacks.picker.command_history() end, desc = "Command history" },
+    { "<leader>sd", function() Snacks.picker.diagnostics_buffer() end, desc = "Diagnostics (buffer)" },
+    { "<leader>sh", function() Snacks.picker.help() end, desc = "Help tags" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jump list" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+    { "<leader>sM", function() Snacks.picker.man() end, desc = "Man pages" },
+    { "<leader>sn", function() Snacks.notifier.show_history() end, desc = "Notification history" },
+    { "<leader>sp", function() Snacks.picker.pickers() end, desc = "Pickers" },
+    { "<leader>sr", function() Snacks.picker.registers() end, desc = "Registers" },
     { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume last picker" },
-    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart find files" },
-    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
-    { "<leader>e", function() Snacks.picker.explorer() end, desc = "File explorer" },
     { "<leader>ss", function() Snacks.picker.treesitter() end, desc = "Treesitter symbols" },
-    { "<leader>sb", function() Snacks.picker.lines() end, desc = "Search buffer" },
+    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo history" },
     { "<leader>sz", function() Snacks.picker.spelling() end, desc = "Spell suggestions" },
-    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git status" },
+
+    -- Git
     { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git branches" },
+    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git diff hunks" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git log (file)" },
     { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git log" },
     { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git log (line)" },
-    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git log (file)" },
-    { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git diff hunks" },
+    { "<leader>go", function() Snacks.gitbrowse() end, desc = "Open in browser (GitHub)" },
+    { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git status" },
     { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git stash" },
+
+    -- GitHub
     { "<leader>ghi", function() Snacks.picker.gh_issue() end, desc = "GitHub issues" },
     { "<leader>ghp", function() Snacks.picker.gh_pr() end, desc = "GitHub PRs" },
+
     -- LSP
+    { "<leader>li", function() Snacks.picker.lsp_incoming_calls() end, desc = "Incoming calls" },
+    { "<leader>lo", function() Snacks.picker.lsp_outgoing_calls() end, desc = "Outgoing calls" },
     { "<leader>ls", function() Snacks.picker.lsp_symbols() end, desc = "Document symbols" },
     { "<leader>lS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "Workspace symbols" },
-    -- Undo
-    { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo history" },
   },
 }
