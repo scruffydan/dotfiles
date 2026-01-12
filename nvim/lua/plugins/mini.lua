d61119e2d7d8d5084a6fe70ff113e39de984fe53
Dan Moutal <dan@moutal.ca>
2026-01-11 21:19:24 -0800

    nvim: add markdown bold/italic surrounds to mini.surround
    Add custom surroundings for markdown formatting:
    - B for bold (**text**)
    - I for italic (*text*)
    
    Add visual mode shortcuts:
    - Ctrl-b to wrap selection in bold
    - Ctrl-i to wrap selection in italic

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -10,7 +10,26 @@ return {
     -- sa - add surround (saiw) to surround word, sa2j" to surround 2 lines)
     -- sd - delete surround (sd" to delete quotes)
     -- sr - replace surround (sr)" to replace parens with quotes)
-    require("mini.surround").setup({})
+    -- Custom: B for markdown bold (**), I for markdown italic (*)
+    -- Ctrl-b and Ctrl-i in visual mode to add bold/italic
+    require("mini.surround").setup({
+      custom_surroundings = {
+        -- Markdown bold (**)
+        B = {
+          input = { "%*%*().-()%*%*" },
+          output = { left = "**", right = "**" },
+        },
+        -- Markdown italic (*)
+        I = {
+          input = { "%*().-()%*" },
+          output = { left = "*", right = "*" },
+        },
+      },
+    })
+
+    -- Visual mode shortcuts for markdown bold/italic
+    vim.keymap.set("x", "<C-b>", "saB", { remap = true, desc = "Add markdown bold" })
+    vim.keymap.set("x", "<C-i>", "saI", { remap = true, desc = "Add markdown italic" })
 
     -- LSP completion with signature help
     -- Enabled/disabled by completion mode toggle (<leader>tc) in init.lua
