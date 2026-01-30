125221125b1fc44b3fbea885e47959133b975f62
Dan Moutal <dan@moutal.ca>
2026-01-29 20:45:01 -0800

    zsh: fix typos and clean up dead code

--- zsh-install.sh
+++ zsh-install.sh
@@ -5,14 +5,14 @@
 # Creates ~/.zshenv and ~/.zshrc that source dotfiles, then starts zsh
 #
 
-# Set up `.zshenv` file. Appends source `~/dotfile/zsh/env`
+# Set up `.zshenv` file. Appends source `~/dotfiles/zsh/env`
 printf "\
 if [[ -a ~/dotfiles/zsh/env ]]; then
     source ~/dotfiles/zsh/env
 fi
 " >> ~/.zshenv
 
-# Set up `.zshrc` file. Appends source `~/dotfile/zshrc`
+# Set up `.zshrc` file. Appends source `~/dotfiles/zshrc`
 printf "\
 if [[ -a ~/dotfiles/zshrc ]]; then
     source ~/dotfiles/zshrc
