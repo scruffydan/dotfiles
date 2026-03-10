60cbe8c4977ecd9e122315e97e95114bad05d451
Dan Moutal <dan@moutal.ca>
2026-03-09 21:56:23 -0700

    nvim: stop tracking lazy lockfile

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -28,7 +28,6 @@ for _, file in ipairs(vim.fn.readdir(plugins_dir)) do
 end
 
 require("lazy").setup(plugins, {
-  lockfile = vim.g.dotfiles_nvim .. "/lazy-lock.json",
   ui = {
     border = "rounded",
   },
